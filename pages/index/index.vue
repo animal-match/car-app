a482ceeea64dccfc5f93362b5362268908dcefe2
animal-match <wanghao@qq.com>
2021-07-31 00:36:16 +0800

    feat: 绘制底部导航

---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="home">
+		<view class="container">
+			33
+			<!-- 轮播图 -->
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.home {
+		background-color: $uni-bg-color-grey;
+		.container {
+			padding: 0 20rpx 0 20rpx;
+			
+		}
+	}
+</style>
---
+++ pages/require/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>供求页</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
